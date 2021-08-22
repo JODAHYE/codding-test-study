558a65483338426378fee2309be5c00a88177ba9
DAHYE <dahye@gmail.com>
2021-08-22 12:04:23 +0900

    'asdf'

---
+++ 8월/[3주차]파일명정렬.js
@@ -0,0 +1,35 @@
+function solution(files) {
+  // 알파벳(대소문자 같음)=>숫자순(앞0은 상관X)=>알파벳, 숫자 같을 경우 순서 바뀌지 않음
+  let filesSort = files.sort((a, b)=>{
+      if(head(a)>head(b)) return 1;
+      else if(head(a)<head(b)) return -1;
+      else{
+          if(num(a, head(a).length)>num(b, head(b).length)) return 1;
+          else if(num(a, head(a).length)<num(b, head(b).length)) return -1;
+          else return 0;
+      }
+  });
+  return filesSort;
+};
+
+function head(f){
+  let str = f.toLowerCase();
+  let fhead = '';
+  for(let i=0; i<str.length; i++){
+      if((97<=str[i].charCodeAt() && str[i].charCodeAt()<=122)||str[i]===' '||str[i]==='-'||str[i]==='.') fhead+=str[i];
+      else break;
+  }
+  return fhead;
+}
+
+function num(f, hl){
+  let num='';
+  for(let i=hl; i<f.length; i++){
+      if(48<=f[i].charCodeAt() && f[i].charCodeAt()<=57) num+=f[i];
+      else break;
+  }
+  if(num.length>5){
+      num = num.splice(0, 5);
+  } 
+  return Number(num);
+}
